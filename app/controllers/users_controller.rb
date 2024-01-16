6f6446158a83b80e2d4e79e0d119c4ebee05586d
Trevor John <trevor@john.tj>
2024-01-16 09:05:43 -0500

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    @user.webauthn_id = WebAuthn.generate_user_id
     if @user.save
+
+      options = WebAuthn::Credential.options_for_create(
+        user: { id: user.webauthn_id, name: user.name },
+        exclude: user.credentials.map { |c| c.webauthn_id }
+      )
+
+      session[:creation_challenge] = options.challenge
+
       # send email
       redirect_to root_path, notice: "Check your email to complete your sign up"
     else
